0ea3b00e6f745a7e3c622a5600c3dc2e9462d767
e-zk <58356365+e-zk@users.noreply.github.com>
2020-12-16 17:42:36 +1000

    comment config.h

--- config.h
+++ config.h
@@ -1,7 +1,9 @@
 #ifndef XKL_CONFIG_H
 #define XKL_CONFIG_H
 
-static char modsep_char = '+';
+// separator between modifiers + keycode
+static char modsep_char = ':';
+// char to print when modifier is not active
 static char nomod_char = '-';
 
 #endif
